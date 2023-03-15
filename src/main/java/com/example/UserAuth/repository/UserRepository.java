26d04049248614f1103aa89c0c8ee8a4a9df29f7
emrekahraman <43474063+emre-kahraman@users.noreply.github.com>
2023-03-15 23:44:51 +0300

    Update to optional

--- src/main/java/com/example/UserAuth/repository/UserRepository.java
+++ src/main/java/com/example/UserAuth/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.example.UserAuth.repository;
 import com.example.UserAuth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
